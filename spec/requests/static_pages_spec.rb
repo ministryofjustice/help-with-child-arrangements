65352e8fad6b7d75759805a1b0ccd468872f3c5d
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-10-10 11:52:13 +0100

    Add tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,24 @@ RSpec.describe "Static pages" do
   it "shows the accessibility page" do
     get about_accessibility_path
     expect(response).to be_successful
-    expect(response.body).to include("Accessibility statement for Help with child arrangements")
+    expect(response.body).to include("About the accessibility of this online service")
+  end
+
+  it "shows the privacy page" do
+    get about_privacy_path
+    expect(response).to be_successful
+    expect(response.body).to include("Privacy Policy")
+  end
+
+  it "shows the cookies page" do
+    get about_cookies_path
+    expect(response).to be_successful
+    expect(response.body).to include("The cookies we use")
+  end
+
+  it "shows the terms and conditions page" do
+    get about_terms_conditions_path
+    expect(response).to be_successful
+    expect(response.body).to include("By reading this guide you agree to these terms and conditions.")
   end
 end
