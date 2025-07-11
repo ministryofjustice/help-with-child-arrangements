a106a5ee43fcc0f93d8c95844bfa36fcfc313d04
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2025-07-11 12:08:58 +0100

    Amended page content tests.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,91 +3,103 @@ RSpec.describe "Static pages" do
     get root_path
     expect(response).to be_successful
     expect(response.body).to include("Get help with child arrangements")
-    expect(response.body).to include("Find out more about making child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the arbitration page" do
     get arbitration_path
     expect(response).to be_successful
-    expect(response.body).to include("In family arbitration, you and the other person choose a qualified")
-    expect(response.body).to include("Find out more about making child arrangements")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the collaborative law page" do
     get collaborative_law_path
     expect(response).to be_successful
-    expect(response.body).to include("Collaborative lawyers work with you and the other parent to resolve your issues")
-    expect(response.body).to include("Find out more about making child arrangements")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the emotional support page" do
     get emotional_support_path
     expect(response).to be_successful
-    expect(response.body).to include("Emotional support can help you get through the stress of making child arrangements")
-    expect(response.body).to include("Find out more about making child arrangements")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the going to court other parent page" do
     get going_to_court_other_parent_path
     expect(response).to be_successful
-    expect(response.body).to include("The other parent may have told you they will take you to court or you may have received court papers")
-    expect(response.body).to include("Find out how to apply for court order")
+    # expect(response.body).to include("The other parent may have told you they will take you to court or you may have received court papers")
+    # expect(response.body).to include("Find out how to apply for court order")
   end
 
   it "shows the going to court page" do
     get going_to_court_path
     expect(response).to be_successful
-    expect(response.body).to include("You'll need to go to court if you've tried other suitable options and still can't agree")
-    expect(response.body).to include("Find out how to apply for court order")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the involving children page" do
     get involving_children_path
     expect(response).to be_successful
-    expect(response.body).to include("The needs of your children should come first when you make child arrangements")
-    expect(response.body).to include("Find out more about making child arrangements")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the lawyer negotiation page" do
     get lawyer_negotiation_path
     expect(response).to be_successful
-    expect(response.body).to include("You don't have to deal directly with the other parent if you choose this option")
-    expect(response.body).to include("Find out more about making child arrangements")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the negotiating between parents page" do
     get negotiating_between_parents_path
     expect(response).to be_successful
-    expect(response.body).to include("If there are no safety concerns, the cheapest and easiest way to make arrangements")
-    expect(response.body).to include("Get information on making a parenting plan")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the parental responsibility page" do
     get parental_responsibility_path
     expect(response).to be_successful
-    expect(response.body).to include("Parental responsibility is a term that means you have legal rights")
-    expect(response.body).to include("Find out more about parental rights and responsibilities")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the professional mediation page" do
     get professional_mediation_path
     expect(response).to be_successful
-    expect(response.body).to include("Mediation sessions are run by professionals who help you try to reach an agreement")
-    expect(response.body).to include("Find out how to use mediation to make an agreement without going to court")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the professional mediation other parent page" do
     get professional_mediation_other_parent_path
     expect(response).to be_successful
-    expect(response.body).to include("You may have been contacted by a mediator or the other parent may have asked you to try mediation")
-    expect(response.body).to include("Find out how to use mediation to make an agreement without going to court")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the single lawyer page" do
     get single_lawyer_path
     expect(response).to be_successful
-    expect(response.body).to include("One lawyer will work with you and the other parent to manage your separation together without going to court")
-    expect(response.body).to include("Find out more about making child arrangements")
+    expect(response.body).to include("Get help with child arrangements")
+    expect(response.body).to include("This site has been replaced by:")
+    expect(response.body).to include("https://www.gov.uk/looking-after-children-divorce")
   end
 
   it "shows the accessibility page" do
